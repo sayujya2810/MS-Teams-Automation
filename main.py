fcb7314853011aea6abe9445b22a90e01f21d13c
Sayujya Malkan <sayujyamalkan@gmail.com>
2021-11-10 17:16:31 +0530

    pre-final

--- main.py
+++ main.py
@@ -26,10 +26,8 @@ password = os.getenv("PASSWORD")
 emailAddress = os.getenv("EMAIL")
 
 
-
-
 PATH = "C:/Program Files (x86)/chromedriver.exe"
-driver = webdriver.Chrome(executable_path = PATH)
+driver = webdriver.Chrome(executable_path=PATH)
 
 # driver2 = webdriver.Chrome(ChromeDriverManager().install())
 driver.get('https://teams.microsoft.com/go#')
@@ -64,15 +62,19 @@ print("Loading MS Teams...")
 
 className = driver.find_elements_by_class_name("team-card")
 time.sleep(3)
-teamBtn = driver.find_element_by_id("app-bar-2a84919f-59d8-4441-a975-2a8c2643b741")
+teamBtn = driver.find_element_by_id(
+    "app-bar-2a84919f-59d8-4441-a975-2a8c2643b741")
 
 
-def enterClass(day,index):
+def enterClass(day, index):
     className[index].click()
     time.sleep(3)
     print("Entered the class")
+    try:
+        changeChannel(day, index)
+    except:
+        print("No need to change any channel / Some error occured")
 
-    changeChannel(day,index)
     time.sleep(3)
     join_btn = listenForJoinBtn()
     time.sleep(3)
@@ -86,17 +88,20 @@ def enterClass(day,index):
     print('Allowed Cam and Mic permissions')
     time.sleep(2)
     try:
-        cam = driver.find_element_by_css_selector("span[title='Turn camera off']")
+        cam = driver.find_element_by_css_selector(
+            "span[title='Turn camera off']")
         cam.click()
         time.sleep(1)
-        mic = driver.find_element_by_css_selector("span[title='Mute microphone']")
+        mic = driver.find_element_by_css_selector(
+            "span[title='Mute microphone']")
         mic.click()
     except:
         print("Mic and Cam already off")
 
     try:
         time.sleep(2)
-        join = driver.find_element_by_css_selector("button[ng-click='ctrl.joinMeeting()']")
+        join = driver.find_element_by_css_selector(
+            "button[ng-click='ctrl.joinMeeting()']")
         join.click()
     except:
         print("Err in joinings")
@@ -106,13 +111,16 @@ def currentTime():
     t = int(datetime.datetime.now().strftime("%H%M"))
     return t
 
+
 def currentDay():
     d = datetime.datetime.now().strftime("%A")
     return d
 
+
 def wait_until_found(sel, timeout, driver):
     try:
-        element_present = EC.visibility_of_element_located((By.CSS_SELECTOR, sel))
+        element_present = EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, sel))
         WebDriverWait(driver, timeout).until(element_present)
         return driver.find_element_by_css_selector(sel)
     except exceptions.TimeoutException:
@@ -120,26 +128,27 @@ def wait_until_found(sel, timeout, driver):
         return None
 
 
-
-
 def waituntilThis(curr_t, class_time, end_time, index, day):
     curr_t = currentTime()
-    if(curr_t > class_time and curr_t < end_time):
-        enterClass(day,index)
-    if(curr_t > end_time):
-        goHome()
-    while(curr_t < class_time):
+    while curr_t < class_time:
         print("Waiting...")
         time.sleep(3)
+        if curr_t > class_time:
+            break
         curr_t = currentTime()
-        print("Time: ",curr_t)
+        print("Time: ", curr_t)
+        break
+    if(curr_t > class_time and curr_t < end_time):
+        enterClass(day, index)
+    if(curr_t > end_time):
+        goHome()
 
 
-
-def changeChannel(day,index):
+def changeChannel(day, index):
     if(day == "Wednesday" and index == 5):
         time.sleep(3)
-        labChannel = driver.find_element_by_id("channel-19:1IaV-1OEkQRheFliEQ8oaB3Sf6KetAzX5LLDJ2PIvSY1@thread.tacv2")
+        labChannel = driver.find_element_by_id(
+            "channel-19:1IaV-1OEkQRheFliEQ8oaB3Sf6KetAzX5LLDJ2PIvSY1@thread.tacv2")
         labChannel.click()
 
     if(day == "Monday" and index == 7):
@@ -148,11 +157,13 @@ def changeChannel(day,index):
         monChannel.click()
     if(day == "Wednesday" and index == 7):
         time.sleep(3)
-        monChannel = driver.find_element_by_css_selector("a[title = 'Wednesday']")
+        monChannel = driver.find_element_by_css_selector(
+            "a[title = 'Wednesday']")
         monChannel.click()
     if(day == "Thursday" and index == 7):
         time.sleep(3)
-        monChannel = driver.find_element_by_css_selector("a[title = 'Thursday']")
+        monChannel = driver.find_element_by_css_selector(
+            "a[title = 'Thursday']")
         monChannel.click()
 
 
@@ -161,12 +172,15 @@ def goHome():
     time.sleep(2)
     print("You are at home")
 
+
 def listenForJoinBtn():
-    joinBtn = wait_until_found("button[ng-click='ctrl.joinCall()']", 10, driver)
+    joinBtn = wait_until_found(
+        "button[ng-click='ctrl.joinCall()']", 10, driver)
     while(joinBtn is None):
         print("Waiting for class to be started")
         time.sleep(4)
-        joinBtn = wait_until_found("button[ng-click='ctrl.joinCall()']", 10, driver)
+        joinBtn = wait_until_found(
+            "button[ng-click='ctrl.joinCall()']", 10, driver)
     return joinBtn
 
 
@@ -204,37 +218,37 @@ print(d)
 #         print("waiting...")
 
 if(d == "Monday"):
-    waituntilThis(t,951,1130,3,d)
-    waituntilThis(t,1500,1550,8,d)
-    waituntilThis(t,1600,1650,7,d)
-    waituntilThis(t,1700,1750,6,d)
+    waituntilThis(t, 951, 1130, 3, d)
+    waituntilThis(t, 1500, 1550, 8, d)
+    waituntilThis(t, 1600, 1650, 7, d)
+    waituntilThis(t, 1700, 1750, 6, d)
     print("End of the Day brother!!")
 
 if(d == "Tuesday"):
-    waituntilThis(t,1140,1320,9,d)
-    waituntilThis(t,1400,1450,6,d)
-    waituntilThis(t,1500,1550,1,d)
-    waituntilThis(t,1600,1650,4,d)
-    waituntilThis(t,1700,1750,5,d)
+    waituntilThis(t, 957, 1320, 9, d)
+    waituntilThis(t, 1400, 1450, 6, d)
+    waituntilThis(t, 1500, 1550, 1, d)
+    waituntilThis(t, 1600, 1650, 4, d)
+    waituntilThis(t, 1700, 1750, 5, d)
     print("End of the Day brother!!")
 
 
 if(d == "Wednesday"):
-    waituntilThis(t,1700,1750,7,d)
-    waituntilThis(t,1600,1650,8,d)
-    waituntilThis(t,1400,1450,5,d)
-    waituntilThis(t,951,1130,9,d)
+    waituntilThis(t, 1700, 1750, 7, d)
+    waituntilThis(t, 1600, 1650, 8, d)
+    waituntilThis(t, 1400, 1450, 5, d)
+    waituntilThis(t, 951, 1130, 9, d)
     print("End of the Day brother!!")
 
 if(d == "Thursday"):
-    waituntilThis(t,1600,1650,1,d)
-    waituntilThis(t,1500,1550,6,d)
-    waituntilThis(t,1400,1450,7,d)
-    waituntilThis(t,1140,1310,2,d)
+    waituntilThis(t, 1600, 1650, 1, d)
+    waituntilThis(t, 1500, 1550, 6, d)
+    waituntilThis(t, 1400, 1450, 7, d)
+    waituntilThis(t, 1140, 1310, 2, d)
     print("End of the Day brother!!")
 
 if(d == "Friday"):
-    waituntilThis(t,1500,1550,5,d)
-    waituntilThis(t,1400,1450,4,d)
-    waituntilThis(t,1200,1250,10,d)
+    waituntilThis(t, 1500, 1550, 5, d)
+    waituntilThis(t, 1400, 1450, 4, d)
+    waituntilThis(t, 1200, 1250, 10, d)
     print("End of the Day brother!!")
